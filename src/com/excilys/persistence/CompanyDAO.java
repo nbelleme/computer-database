4e2f8a7fe471e20084a7d8421dace3ebec969935
nbelleme <nbelleme@excilys.com>
2016-04-20 17:51:34 +0200

    implement findAll method

--- src/com/excilys/persistence/CompanyDAO.java
+++ src/com/excilys/persistence/CompanyDAO.java
@@ -1,15 +1,23 @@
 package com.excilys.persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.excilys.model.Company;
+import com.excilys.model.Computer;
 
 import mapper.CompanyMapper;
 
 public class CompanyDAO implements DAO<Company> {
     private static final String TABLE = "company";
     private CompanyMapper mapper;
+
+    public CompanyDAO() {
+        mapper = CompanyMapper.getMapper();
+    }
 
     @Override
     public void add(Company item) throws SQLException {
@@ -31,8 +39,19 @@ public class CompanyDAO implements DAO<Company> {
 
     @Override
     public ArrayList<Company> findAll(int firstRow, int countRow) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        try (Connection connection = Database.getConnection()) {
+            String query = "SELECT * " + "FROM " + TABLE + " LIMIT ?,?";
+
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setInt(1, firstRow);
+            stmt.setInt(2, countRow);
+            ResultSet rs = stmt.executeQuery();
+            ArrayList<Company> companies = new ArrayList<Company>();
+            while (rs.next()) {
+                companies.add(mapper.unmap(rs));
+            }
+            return companies;
+        }
     }
 
     @Override
