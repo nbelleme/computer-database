6b498c4b4d2c35455f3db958bd498e5ef5264c27
nbelleme <nbelleme@excilys.com>
2016-04-21 11:23:44 +0200

    add logger

--- src/com/excilys/main/Main.java
+++ src/com/excilys/main/Main.java
@@ -1,11 +1,13 @@
 package com.excilys.main;
 
-import ui.CommandLineInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
-
+    final static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        new CommandLineInterface().run();
+        logger.error("Bonjour");
+//        new CommandLineInterface().run();
     }
 
 }
