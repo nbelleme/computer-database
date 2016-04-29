0097f278466a53d9aa836943256804b4afac7ae4
nbelleme <nbelleme@excilys.com>
2016-04-29 16:04:50 +0200

    add ComputerValidator

---
+++ src/main/java/com/excilys/validator/ComputerValidator.java
@@ -0,0 +1,22 @@
+package com.excilys.validator;
+
+import com.excilys.model.Computer;
+
+public enum ComputerValidator {
+
+  INSTANCE;
+
+  public boolean isValid(Computer computer) {
+    if (computer.getName() != null) {
+      if (computer.getIntroduced() != null) {
+        if (computer.getDiscontinued() != null) {
+          if (computer.getIntroduced().isBefore(computer.getDiscontinued())) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+}
--- src/main/webapp/computer/AddComputer.java
+++ src/main/webapp/computer/AddComputer.java
@@ -24,119 +24,109 @@ import com.excilys.validator.ComputerValidator;
  * Servlet implementation class addComputer
  */
 public class AddComputer extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private CompanyService companyService;
+  private static final long serialVersionUID = 1L;
+  private CompanyService companyService;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AddComputer() {
-        super();
-        companyService = CompanyService.getInstance();
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public AddComputer() {
+    super();
+    companyService = CompanyService.getInstance();
+  }
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    try {
+      ArrayList<Company> companies = (ArrayList<Company>) companyService.findAll();
+      request.setAttribute("companies", companies);
+      request.getRequestDispatcher("/views/addComputer.jsp").forward(request, response);
+    } catch (DaoException e) {
+      request.getRequestDispatcher("/views/500.html").forward(request, response);
     }
+  }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    @Override
-    protected void doGet(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        try {
-            ArrayList<Company> companies = (ArrayList<Company>) companyService
-                    .findAll();
-            request.setAttribute("companies", companies);
-            request.getRequestDispatcher("/views/addComputer.jsp")
-                    .forward(request, response);
-        } catch (DaoException e) {
-            request.getRequestDispatcher("/views/500.html").forward(request,
-                    response);
-        }
-    }
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String name = request.getParameter("computerName");
+    name = name.trim();
+    String introduced = request.getParameter("introduced");
+    introduced = introduced.trim();
+    String discontinued = request.getParameter("discontinued");
+    discontinued = discontinued.trim();
+    String companyId = request.getParameter("companyId");
+    companyId = companyId.trim();
+    PrintWriter out = response.getWriter();
+    out.println(introduced);
+    LocalDateTime introducedDateTime = null;
+    LocalDateTime discontinuedDateTime = null;
+    try {
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    @Override
-    protected void doPost(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        String name = request.getParameter("computerName");
-        name = name.trim();
-        String introduced = request.getParameter("introduced");
-        introduced = introduced.trim();
-        String discontinued = request.getParameter("discontinued");
-        discontinued = discontinued.trim();
-        String companyId = request.getParameter("companyId");
-        companyId = companyId.trim();
-        PrintWriter out = response.getWriter();
-        out.println(introduced);
-        LocalDateTime introducedDateTime = null;
-        LocalDateTime discontinuedDateTime = null;
-        try {
-            DateTimeFormatter formatter = DateTimeFormatter
-                    .ofPattern("yyyyMMdd");
+      if (introduced != "") {
+        if (introduced.contains("/")) {
+          introduced = introduced.replace("/", "");
 
-            if (introduced != "") {
-                if (introduced.contains("/")) {
-                    introduced = introduced.replace("/", "");
-
-                } else {
-                    introduced = introduced.replace("-", "");
-                }
-                LocalDate introducedDate = LocalDate.parse(introduced,
-                        formatter);
-                introducedDateTime = introducedDate.atStartOfDay();
-            }
-            if (discontinued != "") {
-                if (discontinued.contains("/")) {
-                    discontinued = discontinued.replace("/", "");
-
-                } else {
-                    discontinued = discontinued.replace("-", "");
-                }
-                LocalDate discontinuedDate = LocalDate.parse(discontinued,
-                        formatter);
-                discontinuedDateTime = discontinuedDate.atStartOfDay();
-            }
-        } catch (DateTimeException e) {
-            request.setAttribute("errorDate", true);
-            request.getRequestDispatcher("/views/500.html").forward(request,
-                    response);
-        }
-
-        ComputerService computerService = ComputerService.getInstance();
-        CompanyService companyService = CompanyService.getInstance();
-        Company company = null;
-        long idCompany = -1;
-
-        try {
-            idCompany = Long.parseLong(companyId);
-        } catch (NumberFormatException e) {
-
-        }
-
-        try {
-            company = companyService.find(idCompany);
-        } catch (DaoException e) {
-            out.print("company error");
-        }
-        Computer computer = new Computer.Builder().name(name)
-                .introduced(introducedDateTime)
-                .discontinued(discontinuedDateTime).company(company).build();
-
-        if (ComputerValidator.getInstance().isValid(computer)) {
-            try {
-                computerService.add(computer);
-
-            } catch (DaoException e) {
-                out.print(computer.toString());
-                e.printStackTrace();
-            }
         } else {
-            out.println("INVALID MOTHERFUCKER");
+          introduced = introduced.replace("-", "");
         }
+        LocalDate introducedDate = LocalDate.parse(introduced, formatter);
+        introducedDateTime = introducedDate.atStartOfDay();
+      }
+      if (discontinued != "") {
+        if (discontinued.contains("/")) {
+          discontinued = discontinued.replace("/", "");
+
+        } else {
+          discontinued = discontinued.replace("-", "");
+        }
+        LocalDate discontinuedDate = LocalDate.parse(discontinued, formatter);
+        discontinuedDateTime = discontinuedDate.atStartOfDay();
+      }
+    } catch (DateTimeException e) {
+      request.setAttribute("errorDate", true);
+      request.getRequestDispatcher("/views/500.html").forward(request, response);
+    }
+
+    ComputerService computerService = ComputerService.getInstance();
+    CompanyService companyService = CompanyService.getInstance();
+    Company company = null;
+    long idCompany = -1;
+
+    try {
+      idCompany = Long.parseLong(companyId);
+    } catch (NumberFormatException e) {
 
     }
+
+    try {
+      company = companyService.find(idCompany);
+    } catch (DaoException e) {
+      out.print("company error");
+    }
+    Computer computer = new Computer.Builder().name(name).introduced(introducedDateTime)
+        .discontinued(discontinuedDateTime).company(company).build();
+
+    if (ComputerValidator.INSTANCE.isValid(computer)) {
+      try {
+        computerService.add(computer);
+
+      } catch (DaoException e) {
+        out.print(computer.toString());
+        e.printStackTrace();
+      }
+    } else {
+      out.println("INVALID MOTHERFUCKER");
+    }
+
+  }
 
 }
