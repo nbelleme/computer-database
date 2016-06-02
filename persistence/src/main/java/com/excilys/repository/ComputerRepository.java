72e7dd63bb37d1a5a6ec69cccd1a1147cfc633ee
nbelleme <nbelleme@excilys.com>
2016-06-02 18:33:50 +0200

    [SpringSecurity] add security

--- persistence/src/main/java/com/excilys/repository/ComputerRepository.java
+++ persistence/src/main/java/com/excilys/repository/ComputerRepository.java
@@ -1,14 +1,13 @@
 package com.excilys.repository;
 
+import com.excilys.model.Computer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.excilys.model.Computer;
-
 public interface ComputerRepository extends JpaRepository<Computer, Long> {
 
-  Page<Computer> findByNameStartingWithOrCompanyNameStartingWith(String name, String companyName,
-      Pageable page);
+    Page<Computer> findByNameStartingWithOrCompanyNameStartingWith(String name, String companyName,
+                                                                   Pageable page);
 
 }
